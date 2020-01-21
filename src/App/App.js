ed25cf3fae78393aaa441ff15e34290b11fe6dd6
Maggie Greene <maggie.greene96@gmail.com>
2020-01-20 19:56:43 -0600

    Work in Progress

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import './App.scss';
 
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
-        <button className="btn btn-light">Welcome Lady</button>
+        <Router>
+          <Switch>
+            <PublicRoute path="/auth" exact component={authed} authed={authed} />
+            <PrivateRoute path="/" exact component={authed} authed={authed} />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/components/pages/Auth/Auth.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Auth.scss';
+
+class Auth extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Hello</div>
+      </div>
+    );
+  }
+}
+
+export default Auth;
