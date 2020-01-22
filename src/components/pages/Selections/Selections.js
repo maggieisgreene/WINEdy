896284b52ed8678a6989fb553c22beef7bac7dbf
Maggie Greene <maggie.greene96@gmail.com>
2020-01-21 20:52:40 -0600

    Wines print to Selections page per user's selections

--- src/components/pages/Selections/Selections.js
+++ src/components/pages/Selections/Selections.js
@@ -3,33 +3,48 @@ import React from 'react';
 import Selection from '../../shared/Selection/Selection';
 
 import authData from '../../../helpers/data/authData';
+import combinationData from '../../../helpers/data/combinationData';
 import selectionData from '../../../helpers/data/selectionData';
+import wineData from '../../../helpers/data/wineData';
 
 import './Selections.scss';
 
 class Selections extends React.Component {
   state = {
+    combinations: [],
     selections: [],
+    wines: [],
   }
 
-  getSelections = () => {
+  getWinesBySelections = () => {
     selectionData.getSelectionsByUID(authData.getUID())
       .then((selections) => {
-        this.setState({ selections });
+        combinationData.getCombinations()
+          .then((combinations) => {
+            wineData.getWines()
+              .then((wines) => {
+                this.setState({ selections, combinations, wines }); // set state for all once
+              });
+          });
       })
-      .catch((ERR) => console.error('Error from get selections!', ERR));
+      .catch((ERR) => console.error('Error from get selections or combos or wines yee!', ERR));
   }
 
   componentDidMount() {
-    this.getSelections();
+    this.getWinesBySelections();
   }
 
   render() {
+    const { combinations, wines } = this.state;
+
     return (
       <div className="Selections">
-        <h1>Selections</h1>
+        <div className="selections-header">
+          <h2>Your Selections</h2>
+          <button className="btn btn-light">Take Quiz</button>
+        </div>
         <div className="boards d-flex flex-wrap">
-          {this.state.selections.map((selection) => <Selection key={selection.id} board={selection} />)}
+          {this.state.selections.map((selection) => <Selection key={selection.id} combination={combinations.find((x) => x.id === selection.combinationID)} selection={selection} wines={wines} />)}
         </div>
       </div>
     );
--- src/components/shared/Selection/Selection.js
+++ src/components/shared/Selection/Selection.js
@@ -4,9 +4,22 @@ import './Selection.scss';
 
 class Selection extends React.Component {
   render() {
+    const { selection, combination, wines } = this.props;
+
+    const foundWine = wines.find((x) => x.id === combination.wineID);
+
     return (
-      <div className="Selection">
-        <h1>Selection</h1>
+      <div className="Selection col-3">
+        <div className="card">
+        <img src={foundWine.imageURL} className="card-img-top boardImg" alt={selection.name} />
+          <div className="card-body">
+            <h5 className="card-title">{foundWine.name}</h5>
+            <p className="card-text">{foundWine.description}</p>
+            <div className="d-flex justify-content-around">
+              <button className="btn btn-light" onClick={this.deleteBoardEvent}>Delete</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
