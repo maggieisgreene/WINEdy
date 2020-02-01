cd62b99a38f4cf18f7969d7fca0d0a03cc89e704
Maggie Greene <maggie.greene96@gmail.com>
2020-02-01 10:53:04 -0600

    Work in progress

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import MiniSelection from '../../shared/MiniSelection/MiniSelection';
 
 import authData from '../../../helpers/data/authData';
+import candyData from '../../../helpers/data/candyData';
 import combinationData from '../../../helpers/data/combinationData';
 import selectionData from '../../../helpers/data/selectionData';
 import wineData from '../../../helpers/data/wineData';
@@ -12,6 +13,7 @@ import './Dashboard.scss';
 
 class Dashboard extends React.Component {
   state = {
+    candys: [],
     combinations: [],
     selections: [],
     wines: [],
@@ -24,7 +26,15 @@ class Dashboard extends React.Component {
           .then((combinations) => {
             wineData.getWines()
               .then((wines) => {
-                this.setState({ selections, combinations, wines });
+                candyData.getCandys()
+                  .then((candys) => {
+                    this.setState({
+                      selections,
+                      combinations,
+                      wines,
+                      candys,
+                    });
+                  });
               });
           });
       })
@@ -36,10 +46,15 @@ class Dashboard extends React.Component {
   }
 
   render() {
-    const { combinations, selections, wines } = this.state;
+    const {
+      candys,
+      combinations,
+      selections,
+      wines,
+    } = this.state;
 
-    // need way to grab only two or four selections
-    // const randomSelections = selections[Math.floor(Math.random() * selections.length)];
+    const randomCombo = combinations[Math.floor(Math.random() * combinations.length)];
+
 
     return (
       <div className="Dashboard">
@@ -51,17 +66,24 @@ class Dashboard extends React.Component {
           <div className="wine-of-week col-6">
             <div className="weekly-holder">
               <h4>Random wine of the week will go here, hopefully!</h4>
+              <div>
+                { (randomCombo) && candys.find((x) => x.id === randomCombo.candyId) }
+                { (randomCombo) && <p>{randomCombo.candyId}</p> }
+                { (randomCombo) && randomCombo.wineId }
+              </div>
             </div>
           </div>
           <div className="mini-selections col-6">
             <div className="minis-holder">
               <h4>Your Selections</h4>
               <div className="minis">
-              { (!selections.length) && <h4>You need to take the quiz first!</h4>}
-              { (selections.length === 1)
-                ? selections.slice(0, 1).map((selection) => <MiniSelection key={selection.id} combination={combinations.find((x) => x.id === selection.combinationId)} selection={selection} wines={wines} />)
-                : selections.slice(0, 2).map((selection) => <MiniSelection key={selection.id} combination={combinations.find((x) => x.id === selection.combinationId)} selection={selection} wines={wines} />)
-              }
+                { (!selections.length) && <h4>You need to take the quiz first!</h4>}
+                { (selections.length === 1)
+                  ? selections.slice(0, 1).map((selection) => <MiniSelection key={selection.id}
+                  combination={combinations.find((x) => x.id === selection.combinationId)} selection={selection} wines={wines} />)
+                  : selections.slice(0, 2).map((selection) => <MiniSelection key={selection.id}
+                  combination={combinations.find((x) => x.id === selection.combinationId)} selection={selection} wines={wines} />)
+                }
               </div>
               <Link className="btn btn-secondary" to="/selections">See All Selections</Link>
             </div>
